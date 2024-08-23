7ad93d98af23f85d77028082f3a3ddca331a3c92
Leo Gall <gall.dev@proton.me>
2024-08-23 15:05:10 +0200

    tailwind css fix

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.shortcuts import render
-
-urlpatterns = [
-    path('', lambda request: render(request, 'pages/home/home.html'), name='home'),
-    path('impressum/', lambda request: render(request, 'pages/home/impressum.html'), name='impressum'),
-    path('licenses/', lambda request: render(request, 'pages/home/licenses.html'), name='licenses'),
-]
