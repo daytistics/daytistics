7567cdaf868a69e0fdb10270ea3fb8ba891cbb26
David Ikkes <david.ikkes@chisw-dev.com>
2024-09-03 20:27:12 +0300

    added new templates for activities

--- daytistics-core/app/activities/urls.py
+++ daytistics-core/app/activities/urls.py
@@ -5,4 +5,5 @@ app_name = "activities"
 
 urlpatterns = [
     path("", views.get_activities_list, name="list"),
+    path("create", views.create_new_activity, name="create"),
 ]
--- daytistics-core/app/activities/views.py
+++ daytistics-core/app/activities/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.utils.translation import gettext_lazy as _
+
+from app.activities.models import Activity
 
 
+@login_required
 def get_activities_list(request):
     """
     View for the activities list. It renders the activities/dashboard.html template.
@@ -11,10 +18,15 @@ def get_activities_list(request):
     Returns:
         HttpResponse: The response object
     """
-    return render(request, "activities/dashboard.html")
+    activities = Activity.objects.all()
+    return render(request, "activities/dashboard.html", context={
+        "activities": activities
+    })
 
 
-def create_new_activity(request):
+@require_POST
+@login_required
+def create_new_activity(request: HttpRequest):
     """
     View for creating a new activity. It renders the activities/new_activity.html template.
 
@@ -24,4 +36,9 @@ def create_new_activity(request):
     Returns:
         HttpResponse: The response object
     """
-    return render(request, "activities/new_activity.html")
+    print(request.body)
+    print(request.POST.get("name"))
+    return HttpResponse(
+        f"<p class='text-green-500'>{_("Activity created successfully")}</p>",
+        status=201,
+    )
