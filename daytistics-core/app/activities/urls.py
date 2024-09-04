fbfcc9b05e96f6abb9fafe415fcee439b0d5164d
David Ikkes <david.ikkes@chisw-dev.com>
2024-09-04 16:12:53 +0300

    added activity creation baseline

--- daytistics-core/app/activities/urls.py
+++ daytistics-core/app/activities/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "activities"
 
 urlpatterns = [
-    path("", views.get_activities_list, name="list"),
+    path("", views.get_activities_dashboard, name="list"),
     path("create", views.create_new_activity, name="create"),
 ]
--- daytistics-core/app/activities/views.py
+++ daytistics-core/app/activities/views.py
@@ -1,14 +1,15 @@
+from django.db import transaction
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_http_methods
 from django.utils.translation import gettext_lazy as _
 
 from app.activities.models import Activity
 
 
 @login_required
-def get_activities_list(request):
+def get_activities_dashboard(request):
     """
     View for the activities list. It renders the activities/dashboard.html template.
 
@@ -18,7 +19,7 @@ def get_activities_list(request):
     Returns:
         HttpResponse: The response object
     """
-    activities = Activity.objects.all()
+    activities = request.user.all_activities
     return render(request, "activities/dashboard.html", context={
         "activities": activities
     })
@@ -28,7 +29,7 @@ def get_activities_list(request):
 @login_required
 def create_new_activity(request: HttpRequest):
     """
-    View for creating a new activity. It renders the activities/new_activity.html template.
+    View for creating a new activity.
 
     Args:
         request (HttpRequest): The request object
@@ -36,9 +37,41 @@ def create_new_activity(request: HttpRequest):
     Returns:
         HttpResponse: The response object
     """
-    print(request.body)
-    print(request.POST.get("name"))
+    activity_name = request.POST.get("name")
+
+    if not activity_name:
+        return HttpResponse("<p class='text-red-500'>Invalid activity name</p>", status=400)
+
+    existing_activities = Activity.objects.filter(name=activity_name)
+
+    if not existing_activities.exists():
+        with transaction.atomic():
+            activity = Activity(name=activity_name)
+            activity.save()
+            activity.users.add(request.user)
+
+            result_text = "Activity created successfully"
+    else:
+        activity = existing_activities.first()
+        activity.users.add(request.user)
+
+        result_text = "Activity updated successfully"
+
     return HttpResponse(
-        f"<p class='text-green-500'>{_("Activity created successfully")}</p>",
+        f"<p class='text-green-500'>{_(result_text)}</p>",
         status=201,
     )
+
+
+@require_http_methods(["PUT"])
+@login_required
+def edit_activity(request: HttpRequest, activity_id) -> HttpResponse:
+
+    return HttpResponse("", status=204)
+
+
+@require_http_methods(["DELETE"])
+@login_required
+def delete_activity(request: HttpRequest, activity_id) -> HttpResponse:
+
+    return HttpResponse("", status=204)
