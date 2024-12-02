1f5ca85950c207b6dd97da55367c2c4833f90e1c
Leo Gall <gall.dev@proton.me>
2024-12-02 07:09:52 +0100

    Saving files before refreshing line endings

--- backend/daytistics/database.py
+++ backend/daytistics/database.py
@@ -1,10 +1,9 @@
 from sqlmodel import create_engine, SQLModel
+import os
 
 engine = None
 
 
 def setup_db():
-    engine = create_engine(
-        "postgresql://daytisticsdev:daytisticsdev@database:5432/daytisticsdev"
-    )
-    SQLModel.metadata.create_all(engine)
+    global engine
+    engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///db.sqlite"))
