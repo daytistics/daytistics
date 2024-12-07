a854dde3f90066d77f540427dafc113554055c10
Leo Gall <gall.dev@proton.me>
2024-12-07 10:39:34 +0100

    [docs] update docs

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,23 +1,18 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Daytistics',
     description: 'Empowering your daily insights.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
     },
     github: {
       dir: '.starters/default/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'daytistics',
+      owner: 'daytistics',
       edit: true
     },
     aside: {
---
+++ docs/components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 4px;">
+        <img width="40" src="/logo.png" style="height: 40px !important;" />
+        <span style="font-weight: bold; font-size: 1.4rem;">Daytistics</span>
+    </div>
+</template>
