89bbf36578e7eefff6195bf0ce849152a37a853a
Leo Gall <gall.dev@proton.me>
2024-12-15 06:32:23 +0100

    wip: cors and csrf

--- backend/daytistics/main.py
+++ backend/daytistics/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import dotenv
+from asgi_csrf import asgi_csrf
 
 from .integrations.graphql import create_graphql_router
 from .integrations.database import Database
 from .integrations.injections import container, register_dependencies
+from .config import SecurityConfig
 
 
-def create_app() -> FastAPI:
+def create_app():
     dotenv.load_dotenv()
     app = FastAPI()
 
@@ -16,10 +19,25 @@ def create_app() -> FastAPI:
         db = ctx.resolve(Database)
         db.connect()
 
-    graphql_router = create_graphql_router()
-    app.include_router(graphql_router, prefix="/graphql")
+        graphql_router = create_graphql_router()
 
-    return app
+        # Routes
+        app.include_router(graphql_router, prefix="/graphql")
+
+        security_config = ctx.resolve(SecurityConfig)
+
+        # Middlewares
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=security_config.CORS_ALLOW_ORIGINS,
+            allow_credentials=security_config.CORS_ALLOW_CREDENTIALS,
+            allow_methods=security_config.CORS_ALLOW_METHODS,
+            allow_headers=security_config.CORS_ALLOW_HEADERS,
+        )
+
+        return asgi_csrf(
+            app, signing_secret=security_config.SECRET_KEY, always_set_cookie=True
+        )
 
 
 app = create_app()
